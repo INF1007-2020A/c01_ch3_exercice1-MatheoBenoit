57f56a554fbc4dd37bbc4564b98c040936f62ba2
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2020-09-14 17:03:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,12 +4,12 @@
 
 def square_root(number: int) -> float:
     # TODO completer la fonction
-    return square_root **m1/2
+    return square_root ** 1/2
 
 
 def square(number: int) -> int:
     # TODO completer la fonction
-    return square **2
+    return square ** 2
 
 
 def main() -> None:
